6f746b2eec1ef2ce8e0cd4b2f2ed2ac57d84b732
tpodolak <misiek767@gmail.com>
2015-04-02 22:02:39 +0200

    Source code for post http://tomasz-net.blogspot.com/2014/08/nhibernate-generating-where-in-or.html

---
+++ NHibernateDisjunction/NHibernateDisjunction/DataAccessLayer.cs
@@ -0,0 +1,69 @@
+﻿using DAL;
+using FluentNHibernate.Automapping;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using Model.NHibernateAutomappings;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+
+namespace NHibernateAutomappings
+{
+    public class DataAccesLayer
+    {
+        private static DataAccesLayer _instance;
+
+        private DataAccesLayer()
+        {
+
+        }
+
+        private ISessionFactory _sessionFactory;
+
+        private ISessionFactory SessionFactory
+        {
+            get { return _sessionFactory ?? (_sessionFactory = BuildFactory()); }
+        }
+
+        public static DataAccesLayer Instance
+        {
+            get { return _instance ?? (_instance = new DataAccesLayer()); }
+        }
+
+        /// <summary>
+        /// The build factory.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="ISessionFactory"/>.
+        /// </returns>
+        private ISessionFactory BuildFactory()
+        {
+            ISessionFactory buildSessionFactory = Fluently.Configure().Database(
+                MsSqlConfiguration.MsSql2008.ConnectionString(
+                    @"Data Source=TOMEK;Initial Catalog=BlogSamples;Integrated Security=True").ShowSql())
+                .Mappings(val => val.AutoMappings.Add(AutoMap.AssemblyOf<Project>(new DefaultAutomappingConfiguration()).Conventions.Setup(con =>
+                {
+                    con.Add<DefaultTableNameConvention>();
+                    con.Add<DefaultPrimaryKeyConvention>();
+                    con.Add<DefaultStringLengthConvention>();
+                    con.Add<DefaultReferenceConvention>();
+                    con.Add<DefaultHasManyConvention>();
+                })))
+                .ExposeConfiguration(val =>
+                {
+                    val.Properties["format_sql"] = "true";
+                    //uncomment if you want to create database schema based on mappings
+                    // new SchemaExport(val).Execute(true, true, false);
+                })
+                .BuildSessionFactory();
+
+            return
+                buildSessionFactory;
+        }
+
+
+        public ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+    }
+}
---
+++ NHibernateDisjunction/NHibernateDisjunction/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Model.NHibernateAutomappings;
+using NHibernate.Criterion;
+using NHibernateAutomappings;
+
+namespace NHibernateDisjunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrepareData();
+            GetDataUsingDisjunction();
+            GetDataUsingOrRestriction();
+            GetDataUsingIsInExtension();
+        }
+
+        private static void GetDataUsingDisjunction()
+        {
+            using (var session = DataAccesLayer.Instance.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction(IsolationLevel.ReadCommitted))
+                {
+                    try
+                    {
+
+                        var disjunctinos = Restrictions.Disjunction()
+                            .Add(Restrictions.Where<Task>(val => val.Name == "First task"))
+                            .Add(Restrictions.On<Task>(val => val.Id).IsIn(new[] { 1, 2, 3 }));
+
+                        var result = session.QueryOver<Task>().Where(disjunctinos).List();
+
+                    }
+                    catch (Exception)
+                    {
+                        transaction.Rollback();
+                    }
+                    finally
+                    {
+                        session.Close();
+                    }
+                }
+            }
+
+        }
+
+        private static void GetDataUsingOrRestriction()
+        {
+
+            using (var session = DataAccesLayer.Instance.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction(IsolationLevel.ReadCommitted))
+                {
+                    try
+                    {
+
+                        var orRestriction = Restrictions.Or(
+                            Restrictions.Where<Task>(val => val.Name == "First task"),
+                            Restrictions.On<Task>(val => val.Id).IsIn(new[] { 1, 2, 3 }));
+
+                        var result = session.QueryOver<Task>().Where(orRestriction).List();
+
+                    }
+                    catch (Exception)
+                    {
+                        transaction.Rollback();
+                    }
+                    finally
+                    {
+                        session.Close();
+                    }
+                }
+            }
+        }
+
+        private static void GetDataUsingIsInExtension()
+        {
+            using (var session = DataAccesLayer.Instance.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction(IsolationLevel.ReadCommitted))
+                {
+                    try
+                    {
+                        var result =
+                            session.QueryOver<Task>()
+                                   .Where(val => val.Name == "First task" || val.Id.IsIn(new[] { 1, 2, 3 }))
+                                   .List();
+
+                    }
+                    catch (Exception)
+                    {
+                        transaction.Rollback();
+                    }
+                    finally
+                    {
+                        session.Close();
+                    }
+                }
+            }
+        }
+
+        private static void PrepareData()
+        {
+            using (var session = DataAccesLayer.Instance.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction(IsolationLevel.ReadCommitted))
+                {
+                    try
+                    {
+                        var user = new User
+                        {
+                            Name = "Jhon",
+                            Surname = "Kowalski"
+                        };
+                        session.SaveOrUpdate(user);
+                        var project = new Project
+                        {
+                            Name = "Project",
+                            User = user
+                        };
+                        session.SaveOrUpdate(project);
+                        var firstTask = new Task
+                        {
+                            Name = "First task",
+                            Project = project
+                        };
+                        var secondTask = new Task
+                        {
+                            Name = "Second task",
+                            Project = project
+                        };
+                        session.SaveOrUpdate(firstTask);
+                        session.SaveOrUpdate(secondTask);
+                        transaction.Commit();
+                    }
+                    catch (Exception)
+                    {
+                        transaction.Rollback();
+                    }
+                    finally
+                    {
+                        session.Close();
+                    }
+                }
+            }
+        }
+    }
+}
