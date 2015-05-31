a70b4cfbd204bd9644ad5a65f0b36f875dcf3888
tpodolak <misiek767@gmail.com>
2015-05-31 13:37:52 +0200

    added server.jsto host app

---
+++ AngularJSDecorator/server.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+    path = require('path'),
+    app = express();
+
+app.use(express.static(path.join(__dirname, '/app')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/app/index.html'));
+});
+
+var server = app.listen(3000,'127.0.0.1', function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log( path.join(__dirname,'server.js') + ' listening at http://%s:%s', host, port);
+});
