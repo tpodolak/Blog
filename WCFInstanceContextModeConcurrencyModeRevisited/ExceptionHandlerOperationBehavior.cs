8558fcbabe2224c9fd8dd8c62b70ce0e72430b1a
tpodolak <misiek767@gmail.com>
2015-06-29 01:02:22 +0200

    Initial commit

---
+++ WCFInstanceContextModeConcurrencyModeRevisited/ExceptionHandlerOperationBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+
+namespace WCFInstanceContextModeConcurrencyModeRevisited
+{
+    public class ExceptionHandlerOperationBehavior : IOperationBehavior
+    {
+        public void Validate(OperationDescription operationDescription)
+        {
+
+        }
+
+        public void ApplyDispatchBehavior(OperationDescription operationDescription, DispatchOperation dispatchOperation)
+        {
+            dispatchOperation.Invoker = new ExceptionrHandlerOperationInvoker(dispatchOperation.Invoker, dispatchOperation);
+        }
+
+        public void ApplyClientBehavior(OperationDescription operationDescription, ClientOperation clientOperation)
+        {
+        }
+
+        public void AddBindingParameters(OperationDescription operationDescription, BindingParameterCollection bindingParameters)
+        {
+        }
+    }
+}
---
+++ WCFInstanceContextModeConcurrencyModeRevisited/ExceptionHandlerServiceBehaviorAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+
+namespace WCFInstanceContextModeConcurrencyModeRevisited
+{
+    public class ExceptionHandlerServiceBehaviorAttribute : Attribute, IServiceBehavior
+    {
+        public void Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        {
+
+        }
+
+        public void AddBindingParameters(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase, Collection<ServiceEndpoint> endpoints,
+            BindingParameterCollection bindingParameters)
+        {
+            foreach (var operation in endpoints.SelectMany(val => val.Contract.Operations.Where(x => x.Behaviors.Find<ExceptionHandlerOperationBehavior>() == null)))
+            {
+                operation.Behaviors.Add(new ExceptionHandlerOperationBehavior());
+            }
+        }
+
+        public void ApplyDispatchBehavior(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        {
+
+        }
+    }
+}
