88469bfc2ba361b2ed9f1f1a14d2b3764119e5aa
tpodolak <misiek767@gmail.com>
2018-01-06 23:52:48 +0100

    Integration test

--- AspNetCoreManuallyRetrieveSwaggerSchema/AspNetCoreManuallyRetrieveSwaggerSchema.Tests/SchemaRetrieverTests.cs
+++ AspNetCoreManuallyRetrieveSwaggerSchema/AspNetCoreManuallyRetrieveSwaggerSchema.Tests/SchemaRetrieverTests.cs
@@ -1,18 +1,46 @@
+using System.Threading.Tasks;
 using FluentAssertions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
 using Xunit;
+using static System.Uri;
 
 namespace AspNetCoreManuallyRetrieveSwaggerSchema.Tests
 {
     public class SchemaRetrieverTests
     {
+        private readonly SchemaRetriever _subject;
+        private readonly string _swaggerDocumentName;
+
+        public SchemaRetrieverTests()
+        {
+            _subject = new SchemaRetriever();
+            _swaggerDocumentName = "Sample API";
+        }
+
         [Fact]
         public void RetrieveSchema_RetrievesSwaggerSchema()
         {
-            var subject = new SchemaRetriever();
-            
-            var result = subject.RetrieveSchema("Sample API");
+            var result = _subject.RetrieveSchema(_swaggerDocumentName);
 
             result.Should().NotBeNullOrWhiteSpace();
+        }
+
+        [Fact]
+        public async Task RetrieveSchema_RetrievesSwaggerSchema_SameAsDeployedSchema()
+        {
+            var swaggerSchemaWithoutHosting = _subject.RetrieveSchema(_swaggerDocumentName);
+            using (var testServer = new TestServer(new WebHostBuilder().UseStartup<Startup>()))
+            {
+                using (var httpClient = testServer.CreateClient())
+                {
+                    var response = await httpClient.GetAsync($"swagger/{EscapeDataString(_swaggerDocumentName)}/swagger.json");
+                    response.EnsureSuccessStatusCode();
+                    var content = await response.Content.ReadAsStringAsync();
+
+                    swaggerSchemaWithoutHosting.Should().Be(content);
+                }
+            }
         }
     }
 }
